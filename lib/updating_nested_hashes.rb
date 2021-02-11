9683666784d6bf7bf80aa618f38ce78f092519d4
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 20:11:55 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -59,7 +59,7 @@ def add_characters
    # Code your solution here:
    epic_tragedy{
      :additional_characters => [
-       {:name => "Prince Escalus"}
+       {:name => "Prince Escalus"},
        {:name => "Apothecary"}
        ]
    }
